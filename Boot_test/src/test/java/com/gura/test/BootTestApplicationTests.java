856c48f25ae319d79bce1437637606ebb2497190
jangheejin <sl150s@naver.com>
2023-01-02 16:01:00 +0900

    boot07_final 재업로드

---
+++ Boot_test/src/test/java/com/gura/test/BootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gura.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
